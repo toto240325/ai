7ccfb9e1cb60cd9a4d4eda25beb3456094aa9ab4
toto240325 <toto240325@gmail.com>
2022-01-23 14:44:31 +0100

    .

--- bugs.py
+++ bugs.py
@@ -2,13 +2,165 @@ import matplotlib.pyplot as plt
 from time import sleep
 import numpy as np
 import pandas as pd
+from math import log2
 
-data = pd.read_csv("data_bugs.csv")
 
-X = data["Length"].values
-Y = [ (1 if col == "Blue" else (2 if col == "Brown" else 3)) for col in data["Color"]]
-# Y = [ (1 if row["Color"] == "Blue" else (2 if row["Color"] == "Brown" else 3)) for row in data.iterrows()]
-C = [ ("r" if spec == "Lobug" else "b") for spec in data["Species"]]
+# calculate the entryp of this df
+def entropy(df,kind="Species"):
+    kinds = df[kind].unique()
+    total = len(df)
+    nb_per_kind = dict()
+    entropy_per_kind = dict()
+    for k in kinds:
+        nb_per_kind[k] = len(df[(df[kind] == k)])
+        entropy_per_kind[k] = - nb_per_kind[k]/total * log2(nb_per_kind[k]/total) 
+    return sum(entropy_per_kind.values())
+
+
+# calculate information gain if split with given query
+def information_gain_criterion(df, entropy_whole_set, df_query, kind):
+    entropy_subsets = dict()
+    nb_subsets = dict()
+
+    sub_df = df.query(df_query)
+    entropy_subsets[0] = entropy(sub_df,kind)
+    nb_subsets[0] = len(sub_df)
+    # print(f'Entropy for {df_query} : {entropy_subsets[0]}')
+
+    df_query = f"not ({df_query})"
+    sub_df = df.query(df_query)
+    entropy_subsets[1] = entropy(sub_df,kind)
+    nb_subsets[1] = len(sub_df)
+    # print(f'Entropy for {df_query} : {entropy_subsets[1]}')
+
+    return entropy_whole_set - (entropy_subsets[0] * nb_subsets[0] + entropy_subsets[1] * nb_subsets[1] ) / len(df)
+
+
+df = pd.read_csv("data_bugs.csv")
+
+entropy_whole_set = entropy(df,"Species")
+print(f'Entropy whole set : {entropy_whole_set}')
+
+df_query = 'Color == "Green"'
+ig  = information_gain_criterion(df, entropy_whole_set, df_query, "Species")
+print(f'information gain for {df_query} : {ig}')
+
+df_query = 'Color == "Brown"'
+ig  = information_gain_criterion(df, entropy_whole_set, df_query, "Species")
+print(f'information gain for {df_query} : {ig}')
+
+df_query = 'Color == "Blue"'
+ig  = information_gain_criterion(df, entropy_whole_set, df_query, "Species")
+print(f'information gain for {df_query} : {ig}')
+
+df_query = 'Length < 17'
+ig  = information_gain_criterion(df, entropy_whole_set, df_query, "Species")
+print(f'information gain for {df_query} : {ig}')
+
+df_query = 'Length < 20'
+ig  = information_gain_criterion(df, entropy_whole_set, df_query, "Species")
+print(f'information gain for {df_query} : {ig}')
+
+
+exit()
+
+
+# let's split by color and see entropy of resulting sets :
+colors = df["Color"].unique()
+entropy_subsets = dict()
+for g in colors:
+    sub_df = df[(df["Color"]==g)]
+    entropy_subsets[g] = entropy(sub_df,"Species")
+    print(f'Entropy for {g} : {entropy_subsets[g]}')
+info_gain_per_color = entropy_whole_set - sum(entropy_subsets.values())/len(entropy_subsets)
+print(f'information gain if split by color = {info_gain_per_color}')
+
+# let's split by length17 and see entropy of resulting sets :
+entropy_subsets = dict()
+
+sub_df = df[(df["Length"] < 17)]
+entropy_subsets["lessThan17"] = entropy(sub_df,"Species")
+print(f'Entropy for less than 17 : {entropy_subsets["lessThan17"]}')
+
+sub_df = df[(df["Length"] >= 17)]
+entropy_subsets["moreThan17"] = entropy(sub_df,"Species")
+print(f'Entropy for more than 17 : {entropy_subsets["moreThan17"]}')
+
+info_gain_per_length17 = entropy_whole_set - sum(entropy_subsets.values())/len(entropy_subsets)
+print(f'information gain if split by lengh17 = {info_gain_per_length17}')
+
+
+# let's split by length20 and see entropy of resulting sets :
+entropy_subsets = dict()
+
+sub_df = df[(df["Length"] < 20)]
+entropy_subsets["lessThan20"] = entropy(sub_df,"Species")
+print(f'Entropy for less than 20 : {entropy_subsets["lessThan20"]}')
+
+sub_df = df[(df["Length"] >= 20)]
+entropy_subsets["moreThan20"] = entropy(sub_df,"Species")
+print(f'Entropy for more than 20 : {entropy_subsets["moreThan20"]}')
+
+info_gain_per_length20 = entropy_whole_set - sum(entropy_subsets.values())/len(entropy_subsets)
+print(f'information gain if split by lengh20 = {info_gain_per_length20}')
+
+
+
+
+sub_df = df[(df["Color"] == "Green")]
+entropy_subsets["lessThan20"] = entropy(sub_df,"Species")
+print(f'Entropy for less than 20 : {entropy_subsets["lessThan20"]}')
+
+sub_df = df[(df["Color"] >= 20)]
+entropy_subsets["moreThan20"] = entropy(sub_df,"Species")
+print(f'Entropy for more than 20 : {entropy_subsets["moreThan20"]}')
+
+info_gain_per_Color20 = entropy_whole_set - sum(entropy_subsets.values())/len(entropy_subsets)
+print(f'information gain if split by lengh20 = {info_gain_per_Color20}')
+
+
+exit()
+
+
+nb_lobugs = len(df[(df["Species"] =="Lobug")])
+nb_mobugs = len(df[(df["Species"] =="Mobug")])
+nb_bugs = nb_lobugs + nb_mobugs
+entropy_all_bugs = - nb_lobugs/nb_bugs * log2(nb_lobugs/nb_bugs) - nb_mobugs/nb_bugs * log2(nb_mobugs/nb_bugs)
+
+
+
+
+
+exit()
+df = pd.read_csv("data_bugs.csv")
+
+# calculate entropy all bugs
+nb_lobugs = len(df[(df["Species"] =="Lobug")])
+nb_mobugs = len(df[(df["Species"] =="Mobug")])
+
+nb_bugs = nb_lobugs + nb_mobugs
+
+entropy_all_bugs = - nb_lobugs/nb_bugs * log2(nb_lobugs/nb_bugs) - nb_mobugs/nb_bugs * log2(nb_mobugs/nb_bugs)
+
+# calculate entropy if we split by color
+
+colors = df.Color.unique()
+
+nb_bugs_per_color = dict()
+entropy_per_color = dict()
+for c in colors:
+    nb_bugs_per_color[c] = len(df[(df["Color"] ==c)])
+    entropy_per_color[c] = - nb_bugs_per_color[c]/nb_bugs * log2(nb_bugs_per_color[c]/nb_bugs) 
+
+# print(nb_bugs_per_color)
+# print(entropy_per_color)
+
+
+
+X = df["Length"].values
+Y = [ (1 if col == "Blue" else (2 if col == "Brown" else 3)) for col in df["Color"]]
+# Y = [ (1 if row["Color"] == "Blue" else (2 if row["Color"] == "Brown" else 3)) for row in df.iterrows()]
+C = [ ("r" if spec == "Lobug" else "b") for spec in df["Species"]]
 plt.scatter(X,Y,color=C)
 
 plt.show()
